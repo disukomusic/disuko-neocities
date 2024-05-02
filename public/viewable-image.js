8fa7f3bb28b9cf5c97f9ecc2fcf4e84b34f42bc7
Disuko <disukomusic@gmail.com>
2024-05-02 17:29:21 -0400

    collections page wip

---
+++ public/viewable-image.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    const viewableImgs = document.querySelectorAll('.viewable-img');
+
+    viewableImgs.forEach(function(viewableImg) {
+        viewableImg.addEventListener('mouseenter', function() {
+            viewableImg.classList.add('hovered');
+        });
+
+        viewableImg.addEventListener('mouseleave', function() {
+            if (!viewableImg.classList.contains('expanded')) {
+                viewableImg.classList.remove('hovered');
+            }
+        });
+
+        viewableImg.addEventListener('click', function() {
+            if (!viewableImg.classList.contains('expanded')) {
+                closeAllViewableImgs();
+                viewableImg.classList.add('expanded');
+            } else {
+                viewableImg.classList.remove('expanded');
+            }
+        });
+    });
+
+    function closeAllViewableImgs() {
+        viewableImgs.forEach(function(viewableImg) {
+            viewableImg.classList.remove('expanded');
+        });
+    }
+});
