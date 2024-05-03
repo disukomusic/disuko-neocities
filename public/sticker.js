e732aabded02b4e47a28a7643f9f34242a56c16a
Disuko <disukomusic@gmail.com>
2024-05-03 16:32:19 -0400

    STICKERS!!!!

---
+++ public/sticker.js
@@ -0,0 +1,41 @@
+﻿// Make the DIV elements draggable:
+const stickerContainers = document.querySelectorAll('.sticker-container');
+let zIndexCounter = 1;
+
+stickerContainers.forEach(container => {
+    dragElement(container);
+});
+
+function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    const sticker = elmnt.querySelector('.sticker');
+
+    sticker.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Bring the clicked sticker container to the front by increasing its z-index
+        elmnt.style.zIndex = zIndexCounter++;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
