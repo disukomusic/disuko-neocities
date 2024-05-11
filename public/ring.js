30a2bf6c9d29b648cdd3a91d12474dac6ca4cb90
Disuko <disukomusic@gmail.com>
2024-05-11 01:25:56 -0400

    asdfsdfds

--- public/ring.js
+++ public/ring.js
@@ -14,10 +14,10 @@
             stylesheet: "https://your_site_here.neocities.org/folder/widget.css",
             widget: `
     <div id="my-webring" style="display: flex; gap: 8px">
-      <a href="PREV"><img src="https://disuko.neocities.org/images/triangle-right.png" width="32px"></a>
-      <div><img src="https://disuko.neocities.org/images/shiku.png" width="32px"></div>
-      <a href="RANDOM">random</a>
-      <a href="NEXT"><img src="https://disuko.neocities.org/images/triangle-left.png" width="32px"></a>
+      <a href="PREV" class="navbutton"><img src="https://disuko.neocities.org/images/triangle-right.png" width="32px"></a>
+      <div><img src="https://disuko.neocities.org/images/shiku.png" class="shiku" width="32px"></div>
+      <a href="RANDOM" class="navbutton">random</a>
+      <a href="NEXT" class="navbutton"><img src="https://disuko.neocities.org/images/triangle-left.png" width="32px"></a>
     </div>
   `,
 
@@ -31,7 +31,7 @@
     webring.index = location.href.startsWith("file://") ? 0 : webring.sites.findIndex(url => location.href.startsWith(url));
     if (webring.index == -1) document.currentScript.outerHTML = webring.error;
     else {
-        let sheet = document.createElement("link");
+        let sheet = document.createElement("https://disuko.neocities.org/RPSWebring.css");
         sheet.rel = "stylesheet", sheet.href = webring.stylesheet;
         document.head.appendChild(sheet);
         webring.widget = webring.widget.replace("PREV", webring.sites.at(webring.index - 1));
