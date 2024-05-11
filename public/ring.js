788422be629237754d61f7ef795d8b1b4c49ac3a
Disuko <disukomusic@gmail.com>
2024-05-11 01:24:03 -0400

    Update ring.js

--- public/ring.js
+++ public/ring.js
@@ -1,33 +1,42 @@
-﻿// sitelist (newest to oldest)
-// strip the https:// off
-//based on https://webcatz.neocities.org/webstring/
-RPSWebringSites = [
-    "voidreddev.neocities.org",
-    "disuko.neocities.org",
-    "on3ko.lol",
-    "midnightsyntax.neocities.org"
-];
+﻿
 
-// all of the actual code
-RPSWebringIdx = location.href.startsWith("file://") ? 0 : RPSWebringSites.findIndex(url => location.href.startsWith("https://" + url));
-if (RPSWebringIdx == -1) document.currentScript.outerHTML = `<div>
-          <a href="https://disuko.neocities.org" target="_top"><img src="https://disuko.neocities.org/images/shiku.png"></a>
-          <div style="font-size: 0.75em; text-align: center">This site isn't part of the RED PANDA NETWORK webring yet.</div>
-        </div>`;
-else {
-    let widget = `
-            <div class="webringbox">
-                <div id="prevSiteBtn" class="navbutton"><a href="https://${RPSWebringSites[(RPSWebringIdx - 1 + RPSWebringSites.length) % RPSWebringSites.length]}" target="_top"><img src="../images/triangle-right.png" width="16px"></a></div> 
-                <span id="webringText" class="webringTextContainer">
-                    <a href="https://redpandastudios.net" target="_parent">
-                        <img class="shiku" src="../images/shiku.png" width="64px">
-                    </a>
-                </span> 
-                <div id="nextSiteBtn" class="navbutton"><a href="https://${RPSWebringSites[(RPSWebringIdx + 1) % RPSWebringSites.length]}" target="_top"><img src="../images/triangle-left.png" width="16px"></a></div>
-            </div>
-          `;
-    if (document.currentScript.dataset.widget === "nb") widget = widget.replaceAll("/widget/trans/", "/widget/nb/");
-    document.currentScript.outerHTML = widget;
-}
-delete RPSWebringSites;
-delete RPSWebringIdx;
+        // webstring by june @ webcatz.neocities.org
+
+// webring settings
+        webring = {
+            sites: [
+                "https://disuko.neocities.org",
+                "https://voidreddev.neocities.org",
+                "https://on3ko.lol",
+                "https://midnightsyntax.neocities.org",
+            ],
+            // PREV and NEXT get replaced with neighboring site urls
+            stylesheet: "https://your_site_here.neocities.org/folder/widget.css",
+            widget: `
+    <div id="my-webring" style="display: flex; gap: 8px">
+      <a href="PREV"><img src="https://disuko.neocities.org/images/triangle-right.png" width="32px"></a>
+      <div><img src="https://disuko.neocities.org/images/shiku.png" width="32px"></div>
+      <a href="RANDOM">random</a>
+      <a href="NEXT"><img src="https://disuko.neocities.org/images/triangle-left.png" width="32px"></a>
+    </div>
+  `,
+
+            error: "<div>This site isn't part of the webring yet.</div>",
+
+        };
+
+
+
+    // all of the actual code
+    webring.index = location.href.startsWith("file://") ? 0 : webring.sites.findIndex(url => location.href.startsWith(url));
+    if (webring.index == -1) document.currentScript.outerHTML = webring.error;
+    else {
+        let sheet = document.createElement("link");
+        sheet.rel = "stylesheet", sheet.href = webring.stylesheet;
+        document.head.appendChild(sheet);
+        webring.widget = webring.widget.replace("PREV", webring.sites.at(webring.index - 1));
+        webring.widget = webring.widget.replace("NEXT", webring.sites[(webring.index + 1) % webring.sites.length]);
+        webring.widget = webring.widget.replace("RANDOM", webring.sites[Math.floor(Math.random() * webring.sites.length)]);
+        document.currentScript.outerHTML = webring.widget;
+    }
+    delete webring;
