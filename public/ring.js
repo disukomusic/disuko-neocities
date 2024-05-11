d15053bd52d72717976672fe6c710a6e5162b62c
Disuko <disukomusic@gmail.com>
2024-05-11 01:17:42 -0400

    asfdfds

--- public/ring.js
+++ public/ring.js
@@ -1,27 +1,21 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>RED PANDA NETWORK</title>
-    <link href="../RPSWebring.css" media="all" rel="stylesheet" type="text/css">
-    <script>
-        // sitelist (newest to oldest)
-        // strip the https:// off
-        RPSWebringSites = [
-            "voidreddev.neocities.org",
-            "disuko.neocities.org",
-            "on3ko.lol",
-            "midnightsyntax.neocities.org"
-        ];
+﻿// sitelist (newest to oldest)
+// strip the https:// off
+//based on https://webcatz.neocities.org/webstring/
+RPSWebringSites = [
+    "voidreddev.neocities.org",
+    "disuko.neocities.org",
+    "on3ko.lol",
+    "midnightsyntax.neocities.org"
+];
 
-        // all of the actual code
-        RPSWebringIdx = location.href.startsWith("file://") ? 0 : RPSWebringSites.findIndex(url => location.href.startsWith("https://" + url));
-        if (RPSWebringIdx == -1) document.currentScript.outerHTML = `<div>
+// all of the actual code
+RPSWebringIdx = location.href.startsWith("file://") ? 0 : RPSWebringSites.findIndex(url => location.href.startsWith("https://" + url));
+if (RPSWebringIdx == -1) document.currentScript.outerHTML = `<div>
           <a href="https://disuko.neocities.org" target="_top"><img src="https://disuko.neocities.org/images/shiku.png"></a>
           <div style="font-size: 0.75em; text-align: center">This site isn't part of the RED PANDA NETWORK webring yet.</div>
         </div>`;
-        else {
-            let widget = `
+else {
+    let widget = `
             <div class="webringbox">
                 <div id="prevSiteBtn" class="navbutton"><a href="https://${RPSWebringSites[(RPSWebringIdx - 1 + RPSWebringSites.length) % RPSWebringSites.length]}" target="_top"><img src="../images/triangle-right.png" width="16px"></a></div> 
                 <span id="webringText" class="webringTextContainer">
@@ -32,13 +26,8 @@
                 <div id="nextSiteBtn" class="navbutton"><a href="https://${RPSWebringSites[(RPSWebringIdx + 1) % RPSWebringSites.length]}" target="_top"><img src="../images/triangle-left.png" width="16px"></a></div>
             </div>
           `;
-            if (document.currentScript.dataset.widget === "nb") widget = widget.replaceAll("/widget/trans/", "/widget/nb/");
-            document.currentScript.outerHTML = widget;
-        }
-        delete RPSWebringSites;
-        delete RPSWebringIdx;
-    </script>
-</head>
-<body>
-</body>
-</html>
+    if (document.currentScript.dataset.widget === "nb") widget = widget.replaceAll("/widget/trans/", "/widget/nb/");
+    document.currentScript.outerHTML = widget;
+}
+delete RPSWebringSites;
+delete RPSWebringIdx;
