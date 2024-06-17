722543600724171a0fe8197e96a81d24f480d772
Disuko <disukomusic@gmail.com>
2024-06-16 21:55:15 -0400

    new intro page

---
+++ public/parallax.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('mousemove', function(e) {
+    var x = (e.clientX / window.innerWidth) - 0.5;
+    var y = (e.clientY / window.innerHeight) - 0.5;
+    var elements = document.querySelectorAll('.parallax-element');
+
+    elements.forEach(function(element) {
+        var depth = element.getAttribute('data-depth') || 1;
+        var moveX = (x * depth * 30).toFixed(2);
+        var moveY = (y * depth * 30).toFixed(2);
+        element.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    });
+});
